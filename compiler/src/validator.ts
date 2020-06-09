710167d3871a73374db079f121245fdfb9367a91
Evin Sellin <evinism@gmail.com>
2020-06-08 21:50:08 -0700

    put duplicate detection in validation step

--- compiler/src/validator.ts
+++ compiler/src/validator.ts
@@ -1,8 +1,15 @@
-import { SurpcFile, Reference, Service } from "./ast";
+import {
+  SurpcFile,
+  Reference,
+  Service,
+  VariableDeclaration,
+  Channel,
+} from "./ast";
+import { serviceParser } from "./parser";
 
 // This enforces some constraints on Sur files.
-// These constraints shouldn't be considered part of the ast because they're
-// likely to change! Is that a good choice? Idk. It seems reasonable to me.
+// These constraints shouldn't be considered part of the ast because they don't
+// represent the syntax structure!
 export function validate(file: SurpcFile<Reference>): SurpcFile<Reference> {
   // Constraint 1: Top level var declarations should not include channel and rpc
   // definitions
@@ -26,6 +33,24 @@ export function validate(file: SurpcFile<Reference>): SurpcFile<Reference> {
   if (!noNestedServices) {
     throw `Cannot nest services`;
   }
+
+  // Constraint 3: There can't be repeated variable names
+  const ensureNoRepeats = (variables: VariableDeclaration<unknown>[]) => {
+    const acc: { [key: string]: boolean } = {};
+    for (let { name } of variables) {
+      if (acc[name]) {
+        throw new Error(`Repeated variable ${name} in ${file.path}`);
+      }
+      acc[name] = true;
+    }
+  };
+
+  ensureNoRepeats(file.variables);
+  file.variables
+    .filter((decl) => decl.value.type === "service")
+    .forEach((decl) => {
+      ensureNoRepeats((decl.value as Service<unknown>).variables);
+    });
 
   return file;
 }
