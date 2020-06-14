3a386795ff6314d3e42e923c8adb92b6c16a0f21
Evin Sellin <evinism@gmail.com>
2020-06-14 14:05:12 -0700

    reenable tests

--- compiler/src/tests/resolve.spec.ts
+++ compiler/src/tests/resolve.spec.ts
@@ -280,9 +280,398 @@ describe("Resolving", function () {
   });
 
   describe("Multiple Files", function () {
-    it.skip("resolves vars in party correctly", function () {
+    it("resolves vars in party correctly", function () {
       const output = load("../data/resolve/party/main.sur");
-      chai.assert.deepEqual(output, {});
+      chai.assert.deepEqual(output, {
+        path: "../data/resolve/party/main.sur",
+        imports: [],
+        variables: [
+          {
+            statementType: "declaration",
+            name: "Slot",
+            value: {
+              type: "struct",
+              fields: [
+                {
+                  name: "people",
+                  optional: false,
+                  baseType: {
+                    type: "list",
+                    value: {
+                      type: "struct",
+                      fields: [
+                        {
+                          name: "name",
+                          optional: false,
+                          baseType: {
+                            type: "string",
+                          },
+                        },
+                        {
+                          name: "pronouns",
+                          optional: false,
+                          baseType: {
+                            type: "list",
+                            value: {
+                              type: "string",
+                            },
+                          },
+                        },
+                      ],
+                    },
+                  },
+                },
+                {
+                  name: "startTime",
+                  optional: false,
+                  baseType: {
+                    type: "integer",
+                  },
+                },
+                {
+                  name: "endTime",
+                  optional: false,
+                  baseType: {
+                    type: "integer",
+                  },
+                },
+              ],
+            },
+          },
+          {
+            statementType: "declaration",
+            name: "AddToPartyResponse",
+            value: {
+              type: "struct",
+              fields: [
+                {
+                  name: "success",
+                  optional: false,
+                  baseType: {
+                    type: "boolean",
+                  },
+                },
+                {
+                  name: "time",
+                  optional: true,
+                  baseType: {
+                    type: "struct",
+                    fields: [
+                      {
+                        name: "people",
+                        optional: false,
+                        baseType: {
+                          type: "list",
+                          value: {
+                            type: "struct",
+                            fields: [
+                              {
+                                name: "name",
+                                optional: false,
+                                baseType: {
+                                  type: "string",
+                                },
+                              },
+                              {
+                                name: "pronouns",
+                                optional: false,
+                                baseType: {
+                                  type: "list",
+                                  value: {
+                                    type: "string",
+                                  },
+                                },
+                              },
+                            ],
+                          },
+                        },
+                      },
+                      {
+                        name: "startTime",
+                        optional: false,
+                        baseType: {
+                          type: "integer",
+                        },
+                      },
+                      {
+                        name: "endTime",
+                        optional: false,
+                        baseType: {
+                          type: "integer",
+                        },
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+          {
+            statementType: "declaration",
+            name: "ChatMessage",
+            value: {
+              type: "struct",
+              fields: [
+                {
+                  name: "time",
+                  optional: false,
+                  baseType: {
+                    type: "integer",
+                  },
+                },
+                {
+                  name: "content",
+                  optional: false,
+                  baseType: {
+                    type: "string",
+                  },
+                },
+              ],
+            },
+          },
+          {
+            statementType: "declaration",
+            name: "ChatUpdate",
+            value: {
+              type: "struct",
+              fields: [
+                {
+                  name: "time",
+                  optional: false,
+                  baseType: {
+                    type: "integer",
+                  },
+                },
+                {
+                  name: "content",
+                  optional: false,
+                  baseType: {
+                    type: "string",
+                  },
+                },
+                {
+                  name: "author",
+                  optional: false,
+                  baseType: {
+                    type: "struct",
+                    fields: [
+                      {
+                        name: "name",
+                        optional: false,
+                        baseType: {
+                          type: "string",
+                        },
+                      },
+                      {
+                        name: "pronouns",
+                        optional: false,
+                        baseType: {
+                          type: "list",
+                          value: {
+                            type: "string",
+                          },
+                        },
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+          {
+            statementType: "declaration",
+            name: "PartyService",
+            value: {
+              type: "service",
+              name: "PartyService",
+              variables: [
+                {
+                  statementType: "declaration",
+                  name: "AddToParty",
+                  value: {
+                    type: "rpc",
+                    name: "AddToParty",
+                    request: {
+                      name: "request",
+                      optional: false,
+                      baseType: {
+                        type: "struct",
+                        fields: [
+                          {
+                            name: "name",
+                            optional: false,
+                            baseType: {
+                              type: "string",
+                            },
+                          },
+                          {
+                            name: "pronouns",
+                            optional: false,
+                            baseType: {
+                              type: "list",
+                              value: {
+                                type: "string",
+                              },
+                            },
+                          },
+                        ],
+                      },
+                    },
+                    response: {
+                      name: "response",
+                      optional: false,
+                      baseType: {
+                        type: "struct",
+                        fields: [
+                          {
+                            name: "success",
+                            optional: false,
+                            baseType: {
+                              type: "boolean",
+                            },
+                          },
+                          {
+                            name: "time",
+                            optional: true,
+                            baseType: {
+                              type: "struct",
+                              fields: [
+                                {
+                                  name: "people",
+                                  optional: false,
+                                  baseType: {
+                                    type: "list",
+                                    value: {
+                                      type: "struct",
+                                      fields: [
+                                        {
+                                          name: "name",
+                                          optional: false,
+                                          baseType: {
+                                            type: "string",
+                                          },
+                                        },
+                                        {
+                                          name: "pronouns",
+                                          optional: false,
+                                          baseType: {
+                                            type: "list",
+                                            value: {
+                                              type: "string",
+                                            },
+                                          },
+                                        },
+                                      ],
+                                    },
+                                  },
+                                },
+                                {
+                                  name: "startTime",
+                                  optional: false,
+                                  baseType: {
+                                    type: "integer",
+                                  },
+                                },
+                                {
+                                  name: "endTime",
+                                  optional: false,
+                                  baseType: {
+                                    type: "integer",
+                                  },
+                                },
+                              ],
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  },
+                },
+                {
+                  statementType: "declaration",
+                  name: "Chat",
+                  value: {
+                    type: "channel",
+                    name: "Chat",
+                    incoming: {
+                      name: "incoming",
+                      optional: false,
+                      baseType: {
+                        type: "struct",
+                        fields: [
+                          {
+                            name: "time",
+                            optional: false,
+                            baseType: {
+                              type: "integer",
+                            },
+                          },
+                          {
+                            name: "content",
+                            optional: false,
+                            baseType: {
+                              type: "string",
+                            },
+                          },
+                        ],
+                      },
+                    },
+                    outgoing: {
+                      name: "outgoing",
+                      optional: false,
+                      baseType: {
+                        type: "struct",
+                        fields: [
+                          {
+                            name: "time",
+                            optional: false,
+                            baseType: {
+                              type: "integer",
+                            },
+                          },
+                          {
+                            name: "content",
+                            optional: false,
+                            baseType: {
+                              type: "string",
+                            },
+                          },
+                          {
+                            name: "author",
+                            optional: false,
+                            baseType: {
+                              type: "struct",
+                              fields: [
+                                {
+                                  name: "name",
+                                  optional: false,
+                                  baseType: {
+                                    type: "string",
+                                  },
+                                },
+                                {
+                                  name: "pronouns",
+                                  optional: false,
+                                  baseType: {
+                                    type: "list",
+                                    value: {
+                                      type: "string",
+                                    },
+                                  },
+                                },
+                              ],
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      });
     });
   });
 
@@ -294,7 +683,7 @@ describe("Resolving", function () {
   });
 
   describe("Service descriptors", function () {
-    it.skip("resolves vars in foodDelivery correctly", function () {
+    it("resolves vars in foodDelivery correctly", function () {
       const output = load("../data/resolve/fooddelivery/order.sur");
       chai.assert.deepEqual({}, output);
     });
