1da8d879256a11f5080bfb68c8fd648d54ce7d0a
Evin Sellin <evinism@gmail.com>
2020-06-28 00:33:22 -0700

    bump buttery in snailbook, fix up for auth

--- examples/snailbook/server/src/index.ts
+++ examples/snailbook/server/src/index.ts
@@ -1,20 +1,11 @@
 import { ButteryServer } from "buttery-node";
 import { ensureLoggedIn } from "connect-ensure-login";
-import * as app from "express/lib/application";
-import * as init from "express/lib/middleware/init";
 import "./config";
 //import session = require("express-session");
 import passport = require("passport");
 import { implement } from "./implementations";
 
 const server = new ButteryServer();
-
-const initMiddleware = init.init(app);
-
-server.use((req, res, next) => {
-  console.log(req, res, next);
-  initMiddleware(req, res, next);
-});
 server.use(passport.initialize());
 server.use(ensureLoggedIn("/login"));
 server.use(passport.authenticate("local"));
